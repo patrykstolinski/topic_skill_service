92f6123dd4d2f96739504fcb785797c4e1a4304a
Patryk Stolinski <patrykstolinski@gmail.com>
2025-07-16 13:24:54 +0200

    fix(app.py): Constant PLPG-API-8--10
    - Created constants for skills/topics
    - Swapped written directories for constants
    - Debugging for requested/available IDs implemented

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ import os
 app = Flask(__name__)
 data_manager = JsonDataManager()
 
+DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
+TOPICS_FILE = os.path.join(DATA_DIR, "topics.json")
+SKILLS_FILE = os.path.join(DATA_DIR, "skills.json")
+
 @app.route('/')
 def hello():
     display = "<h1>Hello from Patryk's Topic & Skill Service!</h1>"
@@ -13,7 +17,6 @@ def hello():
     display += "<br>"
     display += '<a href= "http://127.0.0.1:5000/skills">SKILLS</a>'
     return display
-
 
 # def get_topics():
 
@@ -28,13 +31,13 @@ def hello():
 @app.route("/topics")
 def get_topics():
 
-    topics = data_manager.read_data("data/topics.json")
+    topics = data_manager.read_data(TOPICS_FILE)
     return jsonify(topics)
 
 @app.route("/topics/<string:topic_id>")
 def get_topics_by_id(topic_id):
 
-    topics = data_manager.read_data("data/topics.json")
+    topics = data_manager.read_data(TOPICS_FILE)
     #### Debug ###
     print(f"[DEBUG] Requested ID: {topic_id}")
     print(f"[DEBUG] Available ID's: {[topic.get('id') for topic in topics]}")
@@ -48,22 +51,22 @@ def get_topics_by_id(topic_id):
 @app.route("/skills")
 def get_skills():
 
-    skills = data_manager.read_data("data/skills.json")    
+    skills = data_manager.read_data(SKILLS_FILE)    
     return jsonify(skills)
 
 @app.route("/skills/<string:skill_id>")
 def get_skills_by_id(skill_id):
 
-    skills = data_manager.read_data("data/skills.json")
+    skills = data_manager.read_data(SKILLS_FILE)
     #### Debug ####
     print(f"[DEBUG] Requested ID: {skill_id}")
     print(f"[DEBUG] Available ID's: {[skill.get('id') for skill in skills]}")
+
     for skill in skills:
         if skill.get("id").lower() == skill_id.lower():
             return jsonify(skill)
+            
     return jsonify({"Error": "Skill not found."}), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-    
