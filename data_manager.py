12300be04a5db16d8ff019922fb1d28ccac77dee
Patryk Stolinski <patrykstolinski@gmail.com>
2025-07-17 13:11:17 +0200

    PLPG-API-8: <- Refactoring
    - read_data - added check for os.path existence
    - read_data - changed filename to filepath
    - write_data - changed indent to 4
    - write_data - changed filename to filepath

--- data_manager.py
+++ data_manager.py
@@ -7,30 +7,31 @@ class JsonDataManager:
     def __init__(self, baseDir = None):
         # Use the provided base directory or default to the directory of this file
         self.baseDir = baseDir or os.path.dirname(__file__)
-
     
-    def read_data(self, fileName):
-
+    def read_data(self, filepath):
+        
+        if not os.path.exists(filepath):
+            return []
         # Reads JSON data from a file. Returns parsed data if successful, or an empty list on error.
         try: 
-            filePath = os.path.join(self.baseDir, fileName)
-            with open(filePath, "r", encoding="utf-8") as f:
+            with open(filepath, "r", encoding="utf-8") as f:
                 return json.load(f)
         except FileNotFoundError:
-            print(f"[ERROR] File not found: {fileName}")
+            print(f"[ERROR] File not found: {filepath}")
             return []
         except json.JSONDecodeError:
-            print(f"[ERROR] File {fileName} contains invalid JSON.")
+            print(f"[ERROR] File {filepath} contains invalid JSON.")
             return []
 
-    def write_data(self, fileName, data):
 
+    def write_data(self, filepath, data):        
         # Writes JSON data to a file. Returns True if successful, False if data is not serializable.
+        # check if directory exists, if yes, go ahead
+        os.makedirs(os.path.dirname(filepath), exist_ok=True)
         try:
-            filePath = os.path.join(self.baseDir, fileName)
-            with open(filePath, "w", encoding = "utf-8") as f:
-                json.dump(data, f, indent = 2)
-                print(f"[INFO] Successfully wrote to {fileName}")
+            with open(filepath, "w", encoding = "utf-8") as f:
+                json.dump(data, f, indent = 4)
+                print(f"[INFO] Successfully wrote to {filepath}")
                 return True
         except TypeError:
             print(f"[ERROR] Data {data} is not JSON compatible.")
