bb0a9ea32091935628fb98556cb3bed8330b4e61
Patryk Stolinski <patrykstolinski@gmail.com>
2025-07-18 10:45:51 +0200

    PLPG-API-8: Topics by ID as list comprehension

--- app.py
+++ app.py
@@ -20,14 +20,37 @@ def get_topics():
     topics = data_manager.read_data(TOPICS_FILE)
     return jsonify(topics)
 
+# --------------------- topics by ID ---------------
+@app.route('/topics/<string:id>', methods = ['GET'])
+def get_topic_by_id(id):
+    # read the topic file
+    topics = data_manager.read_data(TOPICS_FILE)
+    # for debugging purposes, print the requested and available ID's to the console
+    print(f"[DEBUG] Requested Topic ID: '{id.lower()}'.")
+    print(f"[DEBUG] Available Topic ID's: {[topic.get('id').lower() for topic in topics]}")
+    # List comprehension loop that loops over topics and assigns it to 'topic' - OLD
+    topic = [t for t in topics if t['id'].lower() == id.lower()][0]
+    print(topic)
+    if topic:
+        return jsonify(topic)
+    # if not, return error 
+    return jsonify({"[ERROR]": "Topic not found"}), 404
 
 
 # --------------------- skills ---------------------
-
+    # # loop through ID's to find the requested ID
 @app.route('/skills', methods = ['GET'])
 def get_skills():
     skills = data_manager.read_data(SKILLS_FILE)
     return jsonify(skills)
+
+
+# --------------------- skills by ID ---------------
+@app.route('/skills/<id>', methods = ['GET'])
+def get_skills_by_id(id):
+    skills = data_manager.read_data(SKILLS_FILE)
+    
+
 
 
 if __name__ == '__main__':
