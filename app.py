0bae5607a49f3040816fedc9ea3a1c0832b48edc
Patryk Stolinski <patrykstolinski@gmail.com>
2025-07-18 09:51:48 +0200

    PLPG-API-8: Implemented GET skills endpoint.

--- app.py
+++ app.py
@@ -7,17 +7,27 @@ data_manager = JsonDataManager()
 
 DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
 TOPICS_FILE = os.path.join(DATA_DIR, 'topics.json')
+SKILLS_FILE = os.path.join(DATA_DIR, 'skills.json')
 
 
 @app.route('/')
 def hello_world():
     return "Hello from Topic and Skill Service!"
 
-
+# --------------------- topics ---------------------
 @app.route('/topics', methods=['GET'])
 def get_topics():
     topics = data_manager.read_data(TOPICS_FILE)
     return jsonify(topics)
+
+
+
+# --------------------- skills ---------------------
+
+@app.route('/skills', methods = ['GET'])
+def get_skills():
+    skills = data_manager.read_data(SKILLS_FILE)
+    return jsonify(skills)
 
 
 if __name__ == '__main__':
